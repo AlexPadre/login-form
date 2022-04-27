d61217d8ccf5c84b518c9b4815e67ce693d9a43d
Alexandru Popa <alex.popa@stessa.com>
2022-04-27 11:27:50 +0300

    fix task

--- src/loginService.js
+++ src/loginService.js
@@ -1,15 +1,14 @@
+const admin = {
+  email: "admin@admin.com",
+  password: "admin123"
+}
+
 export const Login = (name, email, password) => {
-
-  const admin = {
-    email: "admin@admin.com",
-    password: "admin123"
-  }
-
-    if (email == admin.email && password == admin.password) {
-      console.log("Logged In");
+    if (email === admin.email && password === admin.password) {
       return {
         name,
         email
       };
     }
+    return null;
   }
